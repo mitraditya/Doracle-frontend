e76b801939626d5f5d4b27e6c09c42268c96fc9b
isha27255 <isha27255@gmail.com>
2021-06-18 21:45:46 +0530

    added custom toggler in patient portal

--- src/PatientPortal/Navbar.js
+++ src/PatientPortal/Navbar.js
@@ -19,7 +19,7 @@ export class Navbar extends Component {
             <nav class="navbar navbar-expand-lg navbar-custom">
             <div class="container-fluid">
                 <NavLink to="/patient" exact activeStyle={{fontWeight: "bold",color: "#104e8b"}} className="navbar-brand"  style={{color: "#104e8b"}}>Doracle</NavLink>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <button class="navbar-toggler custom-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
                 </button>
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
